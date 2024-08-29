3a0d5149358e0155b44b9a4d14bcb0ec5cce35dd
Filmon Ghebremichael <filmo2020gh@gmail.com>
2024-08-29 15:35:03 +0000

    Add PostgreSQL database configuration to Django

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_$354bb5k*-ijiloofsa=1o=n5$p&u&p3e23116o0ft(wr*a9-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
